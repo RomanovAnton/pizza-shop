d8bb8dab68e968847fd74cc5f73e726dcb2d5fba
Anton Romanov <rad250495@yandex.ru>
2022-11-07 17:28:14 +0700

    feat: add readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import "./sass/_main.scss";
@@ -9,11 +9,11 @@ import "./sass/_main.scss";
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
